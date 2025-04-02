7baed0eddc2d4e3c8194c9f469519b943594bca4
Salman <mdsallu040@gmail.com>
2025-04-02 17:14:10 +0530

    for docker purpose

---
+++ src/main/java/com/demo/docker/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.demo.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class HelloWorld {
+
+    @GetMapping("/get")
+    public String getMessage() {
+        return "Hello, Docker World!";
+    }
+}
